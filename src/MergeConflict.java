a80fc5ce2af6eb07351aec53d24887137d64ab6f
John Brinker <john@Johns-MacBook-Pro.local>
2018-08-02 11:44:54 -0400

    added some garbage

--- src/MergeConflict.java
+++ src/MergeConflict.java
@@ -2,7 +2,7 @@
 public class MergeConflict {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello World! lkdsflkdsfkajsdfk");
 
 		System.out.println("I am Groot!");
 	}
